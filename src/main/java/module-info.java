bcc2459f9dd0a47f080eb95879d5052392b65e7d
ssha2504 <163828609+ssha2504@users.noreply.github.com>
2025-02-17 22:33:43 +0100

    finalizar javadoc

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module ies.losmontecillos.temporizadores {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens ies.losmontecillos.temporizadores to javafx.fxml;
-    exports ies.losmontecillos.temporizadores;
-}
